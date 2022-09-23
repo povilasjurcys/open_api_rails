0bbf5bb8727e58a974c2492d7a30f7a915d3d1ba
Povilas Jurcys <po.jurcys@gmail.com>
2022-09-23 07:39:06 +0300

    Initial commit

---
+++ lib/open_api_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_api_rails/version"
+
+module OpenApiRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/open_api_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpenApiRails
+  VERSION = "0.1.0"
+end
